431c5334e8a762eec1fc8fd45c7300b6e5b168f5
zhoushoujian <zhoushoujianwork@163.com>
2024-04-27 20:32:03 +0800

    Remove unnecessary SSH_DIR flag in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-mkdir -p /opt/logs/apps
-# 判断是否有变量SSH_DIR 如果有则带上启动参数
-if [ -n "$SSH_DIR" ]; then
-    # 判断目录是否存在，不存在则创建
-    SSH_DIR_FLAG="--ssh-dir $SSH_DIR"
-    if [ ! -d "$SSH_DIR" ]; then
-        mkdir -p "$SSH_DIR"
-    fi 
-fi
 
 # 判断是否有变量DEBUG 如果有则带上启动参数
 if [ -n "$DEBUG" ]; then
@@ -22,5 +13,5 @@ fi
 if [ -n "$API" ] && [ "$API" = "true" ]; then
     /usr/bin/jms-go api $DEBUG
 else
-    /usr/bin/jms-go sshd $SSH_DIR_FLAG $DEBUG $TIMEOUT
+    /usr/bin/jms-go sshd  $DEBUG $TIMEOUT
 fi
