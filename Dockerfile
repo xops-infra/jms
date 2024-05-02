a27ab970da295a41bd367128c6e643d2f22b5bba
zhoushoujian <zhoushoujianwork@163.com>
2024-05-02 11:25:44 +0800

    Update docker build

--- Dockerfile
+++ Dockerfile
@@ -3,17 +3,15 @@ ENV GOPROXY https://goproxy.cn,direct
 ENV CGO_ENABLED 0
 ENV GOOS linux
 ENV GOARCH amd64
-WORKDIR /opt
-COPY . .
-RUN go mod tidy && go build -o jms-go
-RUN chmod +x /opt/jms-go
+COPY ./bin/jms-linux-amd64 /tmp/
+COPY ./entrypoint.sh /tmp/
+RUN chmod +x /tmp/jms-linux-amd64
+RUN chmod +x /tmp/entrypoint.sh 
 
 FROM amd64/centos:7
 LABEL maintainer="zhoushoujianwork@163.com"
-COPY --from=builder /opt/jms-go /usr/bin/jms-go
-COPY --from=builder /opt/entrypoint.sh /root/entrypoint.sh
+COPY --from=builder /tmp/jms-linux-amd64 /usr/bin/jms-go
+COPY --from=builder /tmp/entrypoint.sh /root/entrypoint.sh
 WORKDIR /root
-RUN chmod +x /root/entrypoint.sh 
-RUN ls -al /root
 EXPOSE 2222
 ENTRYPOINT /root/entrypoint.sh
--- build.sh
+++ build.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 # build for linux&windows&mac
+RELEASE="v1.0.0-beta.$(date +%Y%m%d)"
 
-RELEASE="1.0.0_$(date +%Y%m%d)"
-
-GOOS=darwin GOARCH=arm64 go build -o ./bin/jms-darwin-arm64 -ldflags "-X main.version=$RELEASE"
-
+# GOOS=darwin GOARCH=arm64 go build -o ./bin/jms-darwin-arm64 -ldflags "-X main.version=$RELEASE"
 # GOOS=darwin GOARCH=amd64 go build -o ./bin/jms-darwin-amd64 -ldflags "-X main.version=$RELEASE"
-
 # GOOS=linux GOARCH=amd64 go build -o ./bin/jms-linux-amd64 -ldflags "-X main.version=$RELEASE"
-
+# GOOS=linux GOARCH=arm64 go build -o ./bin/jms-linux-arm64 -ldflags "-X main.version=$RELEASE"
 # GOOS=windows GOARCH=amd64 go build -o ./bin/jms-windows-amd64.exe -ldflags "-X main.version=$RELEASE"
 
-if [ "$1" = "push" ]; then
-    docker build -t zhoushoujian/jms:latest .
+push() {
+    docker build -t zhoushoujian/jms:$RELEASE . --build-arg="RELEASE=$RELEASE"
     if [ $? -eq 0 ]; then
-        echo "build success"
-        # 如果$1为 push 则推送，其他则不推送
-        if [ "$1" = "push" ]; then
-            docker push zhoushoujian/jms:latest
-        fi
+        docker push zhoushoujian/jms:$RELEASE
     else
         echo "build failed"
     fi
+}
+
+if [ "$1" = "push" ]; then
+    push
 fi
